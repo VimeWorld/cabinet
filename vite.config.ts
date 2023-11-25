d74ad26d8483787578ef99e18bb87b9b64733f96
Dmytro Manchynskyi <xtrafrancyz@gmail.com>
2023-11-25 19:34:42 +0200

    Vite 5

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
+        warmup: {
+            clientFiles: ["./src/main.tsx"]
+        },
         proxy: {
             '/api': {
                 target: 'https://cp.vimeworld.com/api',
